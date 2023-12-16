3b7fa27e6687bc9b943222f0cb2641978ea49072
juancespinozazumbado <espinozajuanki@gmail.com>
2023-12-16 07:05:49 -0600

    Add reporitory Pattern

---
+++ src/PosApp.Core/Application/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+using System.Linq.Expressions;
+
+namespace Application.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    public Task<bool> Add(T entity);
+    public Task<IEnumerable<T>> Get(Expression<Func<T, bool>>? filter = null, 
+    Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, string inclideProperties = "");
+    public Task<T?> GetById(string id);
+
+    public Task<bool> Update( T entity);
+
+    public Task<bool> Delete( T entity);
+
+}
